883d6ff8238f5693f25c0658dbffa4f1c1f1fe04
ScheersKarel <karel.scheers@student.kdg.be>
2023-12-29 15:47:27 +0100

    added info on home page

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburgerIcon = document.querySelector('.hamburger-icon');
     const navbar = document.getElementById('navbar');
 
+    if (window.innerWidth > 1024) {
+        navbar.classList.add('active');
+    }
+
     hamburgerIcon.addEventListener('click', function () {
         navbar.classList.toggle('active');
     });
+    
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 1024) {
+            navbar.classList.add('active');
+        } else {
+            navbar.classList.remove('active');
+        }
+    });
+   
 });
