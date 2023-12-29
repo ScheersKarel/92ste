28c45a9ef52dc7d1dcf3113cfc60c0cce4062610
ScheersKarel <karel.scheers@student.kdg.be>
2023-12-29 17:25:54 +0100

    made nav footer en takpage

--- public/home.php
+++ public/home.php
@@ -4,8 +4,82 @@ include('./components/nav.html');
 ?>
 
 <div class="content">
-    
-</div>
+        <img src="./resources/images/imgHome.png" alt="" class="content__img">
+
+        
+
+        <div class="content__container">
+
+            
+            <div class="newscards">
+                <h1 class="content__title"> Nieuwsberichten</h1>
+                <div class="newscards__card">
+                    <div class="newscards__card__header">
+                        <h2 class="newscards__card__header__title">Voetbaltoernooi 2024</h2>
+                        <div class="newscards__card__header__dateButton"><iconify-icon icon="lets-icons:date-today-light" style="color: #053f41;margin-right: 5px;" width="20" height="20"></iconify-icon>08/10</div>
+                        <div class="newscards__card__header__takButtonDesc">Kevins</div>
+                        <div class="newscards__card__header__evenementButtonDesc">Evenement</div>
+                    </div>
+                    <p class="newscards__card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quod maiores? Voluptate doloremque, nisi sequi omnis quas placeat odio dignissimos ex nobis, quibusdam ad officiis! Magni perspiciatis animi earum voluptate.</p>
+                    
+                    <button class="newscards__card__takButton">Kevins</button>
+                    <button class="newscards__card__evenementButton">Evenement</button>
+                    
+                </div>
+
+                <div class="newscards__card">
+                    <div class="newscards__card__header">
+                        <h2 class="newscards__card__header__title">Voetbaltoernooi 2024</h2>
+                        <div class="newscards__card__header__dateButton"><iconify-icon icon="lets-icons:date-today-light" style="color: #053f41;margin-right: 5px;" width="20" height="20"></iconify-icon>08/10</div>
+                        <div class="newscards__card__header__takButtonDesc">Kevins</div>
+                        <div class="newscards__card__header__evenementButtonDesc">Evenement</div>
+                    </div>
+                    <p class="newscards__card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quod maiores? Voluptate doloremque, nisi sequi omnis quas placeat odio dignissimos ex nobis, quibusdam ad officiis! Magni perspiciatis animi earum voluptate.</p>
+                    
+                    <button class="newscards__card__takButton">Kevins</button>
+                    <button class="newscards__card__evenementButton">Evenement</button>
+                    
+                </div>
+
+                <div class="newscards__card">
+                    <div class="newscards__card__header">
+                        <h2 class="newscards__card__header__title">Voetbaltoernooi 2024</h2>
+                        <div class="newscards__card__header__dateButton"><iconify-icon icon="lets-icons:date-today-light" style="color: #053f41;margin-right: 5px;" width="20" height="20"></iconify-icon>08/10</div>
+                        <div class="newscards__card__header__takButtonDesc">Kevins</div>
+                        <div class="newscards__card__header__evenementButtonDesc">Evenement</div>
+                    </div>
+                    <p class="newscards__card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quod maiores? Voluptate doloremque, nisi sequi omnis quas placeat odio dignissimos ex nobis, quibusdam ad officiis! Magni perspiciatis animi earum voluptate.</p>
+                    
+                    <button class="newscards__card__takButton">Kevins</button>
+                    <button class="newscards__card__evenementButton">Evenement</button>
+                    
+                </div>
+
+                <div class="newscards__card">
+                    <div class="newscards__card__header">
+                        <h2 class="newscards__card__header__title">Voetbaltoernooi 2024</h2>
+                        <div class="newscards__card__header__dateButton"><iconify-icon icon="lets-icons:date-today-light" style="color: #053f41;margin-right: 5px;" width="20" height="20"></iconify-icon>08/10</div>
+                        <div class="newscards__card__header__takButtonDesc">Kevins</div>
+                        <div class="newscards__card__header__evenementButtonDesc">Evenement</div>
+                    </div>
+                    <p class="newscards__card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quod maiores? Voluptate doloremque, nisi sequi omnis quas placeat odio dignissimos ex nobis, quibusdam ad officiis! Magni perspiciatis animi earum voluptate.</p>
+                    
+                    <button class="newscards__card__takButton">Kevins</button>
+                    <button class="newscards__card__evenementButton">Evenement</button>
+                    
+                </div>
+
+            </div>
+
+            <div class="info">
+                <H2 class="info__title">Den 92ste</H2>
+                <p class="info__text">
+                    Scouts 92ste scouts Aartselaar - Reet is de enige soutsgroep in de deelgemeente Reet. Elke week zorgen dan ook ongeveer 50 leiders voor boeiende activiteiten voor meer dan 300 leden. <br><br>
+
+                    Onze <a href="">groepsleiding</a> helpt je graag verder met eventuele vragen. Een lijst van onze activiteiten kan je terugvinden in het <a href="">steentje</a>. 
+                </p>
+            </div>
+        </div>
 
 
 
---
+++ public/poemas.php
@@ -0,0 +1,11 @@
+<?php
+include('./components/header.html');
+include('./components/nav.html');
+?>
+
+
+
+<?php
+include('./components/footer.html');
+
+?>
