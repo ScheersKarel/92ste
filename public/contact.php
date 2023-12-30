0bae7b8256ac099d66dfaa5574346c49610b1e69
ScheersKarel <karel.scheers@student.kdg.be>
2023-12-30 11:42:50 +0100

    made contact page

---
+++ public/contact.php
@@ -0,0 +1,92 @@
+<?php
+include('./components/header.html');?>
+<div class="container">
+<?php include('./components/nav.html');?>
+
+
+<div class="content">
+    <img src="./resources/images/imgContact.png" alt="" class="content__img">
+        
+    <div class="content__container">
+        <h1>Contact</h1>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">De groepsleiding</h2>
+            <p class="contactCard__text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <a href="mailto:groepies@92ste.be" class="contactCard__link">groepies@92ste.be</a>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">Wontolla</h2>
+            <p class="contactCard__text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <a href="mailto:wontolla@92ste.be" class="contactCard__link">wontolla@92ste.be</a>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">kafca Admin</h2>
+            <p class="contactCard__text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <a href="mailto:kafca@92ste.be" class="contactCard__link">kafca@92ste.be</a>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">kafca Financiën</h2>
+            <p class="contactCard__text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <a href="mailto:kafcafin@92ste.be" class="contactCard__link">kafcafin@92ste.be</a>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">Ons Gerief</h2>
+            <p class="contactCard__text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <a href="mailto:onsgerief@92ste.be" class="contactCard__link">onsgerief@92ste.be</a>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">Verhuur</h2>
+            <p class="contactCard__text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            </p>
+            <a href="mailto:materiaal@92ste.be" class="contactCard__link">materiaal@92ste.be</a>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">Takken</h2>
+            <a href="mailto:paloebies@92ste.be" class="contactCard__link">paloebies@92ste.be</a><br>
+            <a href="mailto:wolven@92ste.be" class="contactCard__link">wolven@92ste.be</a><br>
+            <a href="mailto:poemas@92ste.be" class="contactCard__link">poemas@92ste.be</a><br>
+            <a href="mailto:jvpv@92ste.be" class="contactCard__link">jvpv@92ste.be</a><br>
+            <a href="mailto:kenners@92ste.be" class="contactCard__link">kenners@92ste.be</a><br>
+            <a href="mailto:jins@92ste.be" class="contactCard__link">jins@92ste.be</a><br>
+        </div>
+
+        <div class="contactCard">
+            <h2 class="contactCard__title">Adres</h2>
+            <p class="contactCard__text">Scouts 92ste</p>
+            <p class="contactCard__text">predikherenhoevestraat 85,</p>
+            <p>2840 Reet</p>
+           
+        </div>
+        
+    </div>
+
+</div>
+
+
+
+<?php 
+include('./components/footer.html');
+?>
+</div>
+</body>
+</html>
+<script src="./resources/js/index.js"></script>
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
