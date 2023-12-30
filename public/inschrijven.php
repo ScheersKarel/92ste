3738a12f62c5c4c01d237601644cee0a9f8dd054
ScheersKarel <karel.scheers@student.kdg.be>
2023-12-30 12:27:36 +0100

    inschrijven page made

---
+++ public/inschrijven.php
@@ -0,0 +1,44 @@
+<?php
+include('./components/header.html');?>
+<div class="container">
+<?php include('./components/nav.html');?>
+
+
+<div class="content">
+        <img src="./resources/images/imgInschijven.png" alt="" class="content__img">
+
+        
+    <div class="content__container">
+
+        <h1>Inschrijvingen 2023-2024</h1>
+        <p>
+        Beste ouder, Hieronder kan u uw zoon inschrijven voor volgend scoutsjaar 2023-2024 (enkel voor nieuwe leden). <br>
+        Opmerking:
+            <ul>
+                <li class="inschrijvingenList">
+                    Indien uw zoon reeds lid is van onze scouts wordt hij automatisch opnieuw ingeschreven. De herinschrijving wordt bevestigd door het storten van het lidgeld. Hierover volgt info per mail.
+                </li>
+                <li class="inschrijvingenList">
+                Nieuwe leden: Gelieve onderaan het formulier, bij opmerkingen, te vermelden of er reeds een broer lid is van onze scouts. Ook graag de volledige naam van beide ouders en gsm-nummers vermelden.
+                </li>
+            </ul>
+        </p>
+
+        <iframe width="950" height="1200" src="https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/client/formulier.html#/lidworden?groep=A3692S" frameborder="0" allowfullscreen></iframe>
+
+    </div>
+
+</div>
+
+
+        
+<?php
+include('./components/footer.html');
+
+?>
+</div>
+
+</body>
+</html>
+<script src="./resources/js/index.js"></script>
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
