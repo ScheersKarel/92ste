2dc49cbccaaf53b11b504506f3ca7c685436eede
ScheersKarel <karel.scheers@student.kdg.be>
2024-01-11 15:57:33 +0100

    added underline on active page + centert steentje

--- public/steentje.php
+++ public/steentje.php
@@ -1,10 +1,9 @@
-
 <?php
-include('./components/header.html');?>
+include('./components/header.html'); ?>
 <div class="container">
-<?php include('./components/nav.html');?>
+    <?php include('./components/nav.html'); ?>
 
-<div class="content">
+    <div class="content">
         <img src="./resources/images/imgSteentje.png" alt="" class="content__img">
         <div class="content__container">
             <div class="steentjeNav">
@@ -24,15 +23,17 @@ include('./components/header.html');?>
         </div>
 
         <div class="steentje">
-            <iframe id="iframe" class="scribd_iframe_embed" title="2de-steentje " src="https://www.scribd.com/embeds/688332614/content?start_page=1&view_mode=scroll&access_key=key-hdZumJ8D6Xjkvw8aMHMr" tabindex="0" data-auto-height="true" data-aspect-ratio="0.7068965517241379" scrolling="no" width="100%" height="600" frameborder="0"></iframe><p  style="   margin: 12px auto 6px auto;   font-family: Helvetica,Arial,Sans-serif;   font-style: normal;   font-variant: normal;   font-weight: normal;   font-size: 14px;   line-height: normal;   font-size-adjust: none;   font-stretch: normal;   -x-system-font: none;   display: block;"   ><a title="View 2de-steentje  on Scribd" href="https://www.scribd.com/document/688332614/2de-steentje#from_embed"  style="text-decoration: underline;">2de-steentje </a> by <a title="View scouts92ste's profile on Scribd" href="https://www.scribd.com/user/147864658/scouts92ste#from_embed"  style="text-decoration: underline;">scouts92ste</a></p>
+            <iframe id="iframe" class="scribd_iframe_embed" title="2de-steentje " src="https://www.scribd.com/embeds/688332614/content?start_page=1&view_mode=scroll&access_key=key-hdZumJ8D6Xjkvw8aMHMr" tabindex="0" data-auto-height="true" data-aspect-ratio="0.7068965517241379" scrolling="no" width="100%" height="600" frameborder="0"></iframe>
+            <p style="   margin: 12px auto 6px auto;   font-family: Helvetica,Arial,Sans-serif;   font-style: normal;   font-variant: normal;   font-weight: normal;   font-size: 14px;   line-height: normal;   font-size-adjust: none;   font-stretch: normal;   -x-system-font: none;   display: block;"><a title="View 2de-steentje  on Scribd" href="https://www.scribd.com/document/688332614/2de-steentje#from_embed" style="text-decoration: underline;">2de-steentje </a> by <a title="View scouts92ste's profile on Scribd" href="https://www.scribd.com/user/147864658/scouts92ste#from_embed" style="text-decoration: underline;">scouts92ste</a></p>
         </div>
-</div>
+    </div>
 
-<?php 
-include('./components/footer.html');
-?>
+    <?php
+    include('./components/footer.html');
+    ?>
 </div>
 </body>
+
 </html>
 <script src="./resources/js/index.js"></script>
 <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburgerIcon = document.querySelector('.hamburger-icon');
     const navbar = document.getElementById('navbar');
 
+    // Functie om de huidige pagina te markeren
+    function markCurrentPage() {
+        const currentPage = window.location.pathname.split("/").pop();
+        const links = document.querySelectorAll("#navbar a");
+
+        for (let i = 0; i < links.length; i++) {
+            const link = links[i];
+            const linkPage = link.href.split("/").pop();
+
+            if (currentPage === linkPage) {
+
+                if (currentPage === "contact" || currentPage === "voetreflexologie" || currentPage === "vertebraal-correctie" || currentPage === "psychozone-harmonisering") {
+                    link.style.textDecoration = 'underline';
+                }
+
+                else {
+                    link.style.textDecoration = 'underline';
+                    link.style.color = '#10587B';
+                }
+            }
+            // Check if the current page is the home page
+            else if (currentPage === "" && linkPage === "voetGevoel") {
+                link.style.textDecoration = 'underline';
+                link.style.color = '#10587B';
+            }
+        }
+    }
+
     if (window.innerWidth > 1024) {
         navbar.classList.add('active');
+        markCurrentPage(); // Markeer de huidige pagina als de site wordt geladen
     }
 
     hamburgerIcon.addEventListener('click', function () {
         navbar.classList.toggle('active');
     });
-    
+
     window.addEventListener('resize', function () {
         if (window.innerWidth > 1024) {
             navbar.classList.add('active');
+            markCurrentPage(); // Markeer de huidige pagina bij het wijzigen van de schermgrootte
         } else {
             navbar.classList.remove('active');
         }
     });
-   
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -33,27 +62,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Voeg een klikgebeurtenis toe aan de knoppen
     agendaButton.addEventListener('click', function () {
-       // alert('agenda button is pressed');
-        scrollToPage(3); 
+        // alert('agenda button is pressed');
+        scrollToPage(3);
     });
     onsGeriefButton.addEventListener('click', function () {
-       // alert('Ons Gerief button is pressed');
-        scrollToPage(4); 
+        // alert('Ons Gerief button is pressed');
+        scrollToPage(4);
     });
     paloebiesButton.addEventListener('click', function () {
-       // alert('paloebie button is pressed');
+        // alert('paloebie button is pressed');
         scrollToPage(9);
     });
     wolvenButton.addEventListener('click', function () {
-       // alert('wolven button is pressed');
+        // alert('wolven button is pressed');
         scrollToPage(13);
     });
     poemaButton.addEventListener('click', function () {
-       // alert('pema button is pressed');
+        // alert('pema button is pressed');
         scrollToPage(15);
     });
     jvpvButton.addEventListener('click', function () {
-       // alert('jvpv button is pressed');
+        // alert('jvpv button is pressed');
         scrollToPage(20);
     });
     kevinButton.addEventListener('click', function () {
@@ -64,8 +93,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Functie om naar een specifieke pagina te springen
     function scrollToPage(page) {
-            document.querySelector('iframe').src = document.querySelector('iframe').src.replace(/page=.*&/,'page='+page+'&');
-            console.log(document.querySelector('iframe').src);
-        }
-    
+        document.querySelector('iframe').src = document.querySelector('iframe').src.replace(/page=.*&/, 'page=' + page + '&');
+        console.log(document.querySelector('iframe').src);
+    }
+
 });
