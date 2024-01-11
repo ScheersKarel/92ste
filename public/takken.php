c4eb8a92788c9be952a56c03f6ce3e8f6575221b
ScheersKarel <karel.scheers@student.kdg.be>
2024-01-11 16:41:30 +0100

    added takken page for mobile

---
+++ public/takken.php
@@ -0,0 +1,59 @@
+<?php
+include('./components/header.html'); ?>
+<div class="container">
+    <?php include('./components/nav.html'); ?>
+
+    <div class="takkenContent">
+        <a href="./paloebies.php">
+            <div class="takkenContent__card">
+                <img class="takkenContent__card__img" src="./resources/images/kapoenen.svg" alt="">
+                <h2>Paloebies</h2>
+            </div>
+        </a>
+
+        <a href="./wolven.php">
+            <div class="takkenContent__card">
+                <img class="takkenContent__card__img" src="./resources/images/wolven.svg" alt="">
+                <h2>wolven</h2>
+            </div>
+        </a>
+
+        <a href="./poemas.php">
+            <div class="takkenContent__card">
+                <img class="takkenContent__card__img" src="./resources/images/poemas.png" alt="">
+                <h2>Poema's</h2>
+            </div>
+        </a>
+
+        <a href="./jvpv.php">
+            <div class="takkenContent__card">
+                <img class="takkenContent__card__img" src="./resources/images/jonggivers.jpg" alt="">
+                <h2>JVPV's</h2>
+            </div>
+        </a>
+
+        <a href="./kevins.php">
+            <div class="takkenContent__card">
+                <img class="takkenContent__card__img" src="./resources/images/givers.jpg" alt="">
+                <h2>Kevins</h2>
+            </div>
+        </a>
+
+        <a href="./jins.php">
+            <div class="takkenContent__card">
+                <img class="takkenContent__card__img" src="./resources/images/jins.png" alt="">
+                <h2>Jins</h2>
+            </div>
+        </a>
+    </div>
+
+
+    <?php
+    include('./components/footer.html');
+    ?>
+</div>
+</body>
+
+</html>
+<script src="./resources/js/index.js"></script>
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
